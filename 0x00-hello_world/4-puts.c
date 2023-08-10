59b04fce6c793a605452861e22cfbac2601a0ad3
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-10 04:08:09 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - prints  "Programming is like building a multilingual puzzle
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
