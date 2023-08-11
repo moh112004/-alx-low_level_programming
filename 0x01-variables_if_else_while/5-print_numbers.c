d43a634952ecdb60aa60cc15a9a34e9d7475f775
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-11 15:01:10 -0700

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints single digits between 0 and 10 
+ * main - prints single digits between 0 and 10
  * after that prints new line
  * Return: always 0
  */
@@ -9,9 +9,9 @@ int main(void)
 {
 int a;
 
-for(a = 0;a<10;a++)
+for (a = 0; a<10; a++)
 {
-printf("%d",a);
+printf("%d", a);
 }
 printf("\n");
 return (0);
