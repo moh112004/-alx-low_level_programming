0c9e82bea407614de232ade8be1b16df5f82b902
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-11 16:33:35 -0700

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ * prints ,
+ * prints white space
+ * after that prints new line
+ * Return: always 0
+ */
+int main(void)
+{
+int a;
+int b;
+for(a = 48 ; a < 58 ; a++)
+{
+for (b = 49 ; b < 58 ; b++)
+{
+if (a == b || a > b){
+continue;
+}
+putchar(a);
+putchar(b);
+if (a == 56 && b == 57){
+continue;
+}
+putchar(',');
+putchar(' ');}
+}
+putchar('\n');
+return (0);
+}
