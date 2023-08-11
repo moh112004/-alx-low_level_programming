59610719156f1845213dc66faba551bc019a553c
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-11 16:11:10 -0700

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ * prints ,
+ * prints white space
+ * after that prints new line
+ * Return: always 0
+ */
+int main(void)
+{
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+if (a != 57)
+{ 
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
