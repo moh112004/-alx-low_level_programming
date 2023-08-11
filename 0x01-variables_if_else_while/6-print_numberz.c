463de420d941e47d878d4b1c2e8648e5878dccec
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-11 15:04:49 -0700

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits between 0 and 10
+ * after that prints new line
+ * Return: always 0
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
