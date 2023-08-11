9cbbf04bb47fef203a45614df8794b8c1d71262d
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-11 16:36:34 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,20 +11,23 @@ int main(void)
 {
 int a;
 int b;
-for(a = 48 ; a < 58 ; a++)
+for (a = 48 ; a < 58 ; a++)
 {
 for (b = 49 ; b < 58 ; b++)
 {
-if (a == b || a > b){
+if (a == b || a > b) 
+{
 continue;
 }
 putchar(a);
 putchar(b);
-if (a == 56 && b == 57){
+if (a == 56 && b == 57) 
+{
 continue;
 }
 putchar(',');
-putchar(' ');}
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
