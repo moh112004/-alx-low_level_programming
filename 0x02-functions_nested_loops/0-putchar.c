acebdca51a89828e6f5215c727c1f8641aa35adf
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-15 05:32:03 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,17 @@
 * prints new line
 * retur 0
 */
-int main( void ){
-char a[] = "_putchar";
-    
-for (int x = 0 ; x < 8 ; x++){
-putchar(a[x]);
-}
-putchar('\n');
-return 0;
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
