bef1b8d8ec5ab55185be28ca98fe73bf0d59f2be
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-14 22:20:49 -0700

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+* print_alphabet - prints alphabet in lower case from a to z
+* main - runs print_alphabet
+* prints new line
+* return 0
+*/
+void print_alphabet() {
+    for (int a = 97; a <= 122; a++) {
+        putchar(a);
+    }
+    putchar('\n');  
+}
+int main() {
+    print_alphabet();
+    return 0;
+}
