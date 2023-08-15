f2239367626d5279534dd3d44681489c29d7f995
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-15 14:49:32 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,18 @@
 * print_alphabet_x10-prints alphabet in lower case ten times
 * Return: 0
 */
-void print_alphabet_x10() {
-int x = 0 ;
+void print_alphabet_x10(void)
+{
+int x = 0;
 int a = 97;
-while ( x < 10 ){
-while ( a <= 122) {
-putchar(a);
+while (x < 10)
+{
+while (a <= 122)
+{
+putchar (a);
 a++;
 }
-putchar('\n');
+putchar ('\n');
 x++;
 a = 97;
 }
