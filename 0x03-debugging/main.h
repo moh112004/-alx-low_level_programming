dd4a671bb7d71a29e5daadfcf9e9d34b0977ec79
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-17 01:06:37 -0700

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,6 @@
+#include <stdio.h>
 
+void positive_or_negative(int n);
+int largest_number(int x, int y, int z);
+int convert_day(int mon, int day);
+void print_remaining_days(int mon, int day, int year);
