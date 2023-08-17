f8e7d4d56399ac8327a3d9ce1af9c832089fb989
moh112004 <118573360+moh112004@users.noreply.github.com>
2023-08-17 22:41:22 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+/**
+* main - tests function that prints is this integer positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
