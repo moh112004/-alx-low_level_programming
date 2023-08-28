4661fa6b001f989d4fba9f96480ef6ffcf215401
Mohammed Elmostafa <118573360+moh112004@users.noreply.github.com>
2023-08-28 22:51:28 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies c bytes from memorey area b
- *  to momery area  a
- *  @a: memory area bytes be cpoied to
- *  @b: memory area bytes be copied from
- *  @c: the number of bytes to copy 
+* *_memcpy - copies c bytes from memorey area b
+*  to momery area  a
+*  @a: memory area bytes be cpoied to
+*  @b: memory area bytes be copied from
+*  @c: the number of bytes to copy
 */
 char *_memcpy(char *a, char *b, unsigned int c)
 {
 unsigned int i;
 
 for ( i = 0; i < c; i++)
-{
-	a[i] = b[i];
-}
-
+a[i] = b[i];
 return a;
-
 }
