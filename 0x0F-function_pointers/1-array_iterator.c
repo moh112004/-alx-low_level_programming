facaf91e1b8a43c18cce0921c339269ca429003c
Mohammed Elmostafa <118573360+moh112004@users.noreply.github.com>
2023-09-14 23:12:53 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
     if(!array || !action)
         return;
-    
+
     for (x = 0; x < size; x++)
         action(array[x]);
 }
