acb6d62887b4d91fae859deae1597e55ba91cc11
Mohammed Elmostafa <118573360+moh112004@users.noreply.github.com>
2023-09-23 10:08:58 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * first - prints a sentence before the main
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
