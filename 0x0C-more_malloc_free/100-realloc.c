2f298f559d0b327834cece1ec2aef8cfe541573c
moh112004 <almstfym870@gmail.com>
2023-09-25 06:31:06 -0700

    update 100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,7 @@
 
 /**
  * *_realloc - a function that is reallcate memory.
+ * @ptr: a pointer to allocated area content.
  * @old_size: the old size of allocated area.
  * @new_size: the new size of allocated area.
  * Return: returns a pointer to allocated area.
@@ -14,15 +15,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int b = 0;
 
 	if (old_size == new_size)
-		return(a);
+		return (a);
 	if (new_size == 0 && ptr != NULL)
 	{
-		free (ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (!ptr)
 		return (malloc(new_size));
-	a = malloc (new_size);
+	a = malloc(new_size);
 	if (!a)
 		return (NULL);
 	if (new_size < old_size)
@@ -31,6 +32,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		for (b = 0; b < old_size; b++)
 			a[b] = ptr[b];
-	free (ptr);
+	free(ptr);
 	return (a);
 }
