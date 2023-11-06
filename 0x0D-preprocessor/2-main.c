9ca93145ca2efea13c23ce6f8636e97fba5776e0
moh112004 <almstym870@gmail.com>
2023-11-06 14:41:44 +0300

    create

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the compiled current file name
+ * Return: returns everything worked will
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
